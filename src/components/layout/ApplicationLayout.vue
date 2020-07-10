4f2ddeafff3da61257934758ff29fcf998b4eb07
GeorgesG9 <georges.abouahmad1@usj.edu.lb>
2020-07-10 16:12:45 +0300

    first commit

--- src/components/layout/ApplicationLayout.vue
+++ src/components/layout/ApplicationLayout.vue
@@ -31,7 +31,7 @@
           ></v-img>
         </v-avatar>
         <div>
-          <v-menu left bottom>
+          <!-- <v-menu left bottom>
               <template v-slot:activator="{ on, attrs }">
                 <v-btn icon v-bind="attrs" v-on="on">
                   <v-avatar >
@@ -52,7 +52,82 @@
                 <v-list-item-title>Option {{ n }}</v-list-item-title>
               </v-list-item>
             </v-list>
-          </v-menu>
+          </v-menu> -->
+
+
+    <!-- GEO -->
+      <v-menu
+        v-model="menu"
+        :close-on-content-click="false"
+        :nudge-width="200"
+        offset-x
+      >
+        <template v-slot:activator="{ on }">
+          <v-btn
+            color="indigo"
+            dark
+            v-on="on"
+          >
+            Menu as Popover
+          </v-btn>
+        </template>
+  
+        <v-card>
+            <div class="col-md-12">
+                adasd
+            </div>
+          <!-- <v-list>
+            <v-list-tile avatar>
+              <v-list-tile-avatar>
+                <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John">
+              </v-list-tile-avatar>
+  
+              <v-list-tile-content>
+                <v-list-tile-title>John Leider</v-list-tile-title>
+                <v-list-tile-sub-title>Founder of Vuetify.js</v-list-tile-sub-title>
+              </v-list-tile-content>
+  
+              <v-list-tile-action>
+                <v-btn
+                  :class="fav ? 'red--text' : ''"
+                  icon
+                  @click="fav = !fav"
+                >
+                  <v-icon>favorite</v-icon>
+                </v-btn>
+              </v-list-tile-action>
+            </v-list-tile>
+          </v-list> -->
+  
+          <v-divider></v-divider>
+  
+          <!-- <v-list>
+            <v-list-tile>
+              <v-list-tile-action>
+                <v-switch v-model="message" color="purple"></v-switch>
+              </v-list-tile-action>
+              <v-list-tile-title>Enable messages</v-list-tile-title>
+            </v-list-tile>
+  
+            <v-list-tile>
+              <v-list-tile-action>
+                <v-switch v-model="hints" color="purple"></v-switch>
+              </v-list-tile-action>
+              <v-list-tile-title>Enable hints</v-list-tile-title>
+            </v-list-tile>
+          </v-list> -->
+  
+          <!-- <v-card-actions>
+            <v-spacer></v-spacer>
+  
+            <v-btn flat @click="menu = false">Cancel</v-btn>
+            <v-btn color="primary" flat @click="menu = false">Save</v-btn>
+          </v-card-actions> -->
+        </v-card>
+      </v-menu>
+
+
+
         </div>
 
     </v-app-bar>
