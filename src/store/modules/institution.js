91b60ad805d2166e933dc88b875347942b8d1fce
GeorgesG9 <georges.abouahmad1@usj.edu.lb>
2020-07-14 16:15:23 +0300

    resolve conflict

--- src/store/modules/institution.js
+++ src/store/modules/institution.js
@@ -60,7 +60,24 @@ export default {
                 })
         })
     },
+    addInstitution({ commit }, institutionRecord) {
 
+        return new Promise((resolve, reject) => {
+
+             axios({ "url": process.env.VUE_APP_SERVER_API + "/institutions", method: "POST" , data: institutionRecord
+                })    
+                .then(resp => {
+                    
+                    /*commit('getInstitution',resp)*/
+                    resolve(resp)
+                    commit(resp);
+                })
+                .catch(err => {
+                    reject(err)
+                })
+            })
+    },
+    
     editInstitution({ commit }, institutionRecord) {
 
         return new Promise((resolve, reject) => {
