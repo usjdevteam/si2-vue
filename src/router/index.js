8337c0ee6fac6f4184a3d81ead509baec740f68e
Sassine Jaoude <sassine.jaoude@hotmail.com>
2020-07-14 14:29:58 -0400

    Update

--- src/router/index.js
+++ src/router/index.js
@@ -1,111 +1,102 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import store from '../store/index.js'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import store from "../store/index.js";
 
-import SignIn from '../components/signIn/SignIn.vue';
-import Application from '../components/layout/ApplicationLayout.vue'
-import Home from '../components/home/Home.vue'
+import SignIn from "../components/signIn/SignIn.vue";
+import Application from "../components/layout/ApplicationLayout.vue";
+import Home from "../components/home/Home.vue";
 
-import AddInstitution from '../components/institutions/AddInstitution.vue'
-import InstitutionIndex from '../components/institutions/Index.vue'
-import ViewInstitution from '../components/institutions/ViewInstitution.vue'
-import EditInstitution from '../components/institutions/EditInstitution.vue'
-
-
+import AddInstitution from "../components/institutions/AddInstitution.vue";
+import InstitutionIndex from "../components/institutions/Index.vue";
+import ViewInstitution from "../components/institutions/ViewInstitution.vue";
+import EditInstitution from "../components/institutions/EditInstitution.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
+    path: "/",
     component: SignIn,
     meta: {
-      requiresAuth: false
-    }
+      requiresAuth: false,
+    },
   },
-  { 
-    path : "/application",
-    name : "Application",
-    component : Application,
-    meta: { 
-        requiresAuth: true
-      },
-    children : [
-
-        {
-            path: 'home',
-            name: 'Home',
-            component: Home,
-            meta: { 
-                requiresAuth: true
-              },
-        },
-        {
-               path: 'institution/add',
-               name: 'AddInstitution',
-               component: AddInstitution,
-               meta: { 
-                   requiresAuth: true
-                 },
-        },
-        {
-          path: 'viewinstitution/:institutionid',
-          name: 'ViewInstitution',
-          props: true ,
-          component: ViewInstitution,
-          meta: { 
-              requiresAuth: true
-            },
-      },  
+  {
+    path: "/application",
+    name: "Application",
+    component: Application,
+    meta: {
+      requiresAuth: true,
+    },
+    children: [
       {
-       path: 'institutions',
-       name: 'Institutions',
-       component: InstitutionIndex,
-       meta: { 
-          requiresAuth: true
-       },
-
+        path: "home",
+        name: "Home",
+        component: Home,
+        meta: {
+          requiresAuth: true,
+        },
+      },
+      {
+        path: "institution/add",
+        name: "AddInstitution",
+        component: AddInstitution,
+        meta: {
+          requiresAuth: true,
+        },
+      },
+      {
+        path: "viewinstitution/:institutionid",
+        name: "ViewInstitution",
+        props: true,
+        component: ViewInstitution,
+        meta: {
+          requiresAuth: true,
+        },
+      },
+      {
+        path: "institutions",
+        name: "Institutions",
+        component: InstitutionIndex,
+        meta: {
+          requiresAuth: true,
+        },
       },
 
       {
-        path: 'institution/:institutionid',
-        name: 'EditInstitution',
-        props: true ,
+        path: "institution/:institutionid",
+        name: "EditInstitution",
+        props: true,
         component: EditInstitution,
-        meta: { 
-            requiresAuth: false
-          },
-      } 
-
-    ]
-  }
+        meta: {
+          requiresAuth: false,
+        },
+      },
+    ],
+  },
 ];
 
 const router = new VueRouter({
-    mode: 'history',
-    base: process.env.BASE_URL,
-    routes,
+  base: process.env.BASE_URL,
+  routes,
 });
 
-
 router.beforeEach((to, from, next) => {
+  if (to.matched.length > 0) {
+    // CHECK IF THE LINK HAS A COMPONENT
 
-
-  if (to.matched.length > 0){ // CHECK IF THE LINK HAS A COMPONENT
-    
-    if(to.matched.some(record => record.meta.requiresAuth)) {
+    if (to.matched.some((record) => record.meta.requiresAuth)) {
       //if (store.getters['session/isLoggedIn'] &&  store.getters['session/authStatus'] == 'success') {
-      if (store.getters['session/isLoggedIn']) {
-        next()
-        return
+      if (store.getters["session/isLoggedIn"]) {
+        next();
+        return;
       }
-      next('/') 
+      next("/");
     }
-    next()
-  }else{
-    next('/') 
+    next();
+  } else {
+    next("/");
   }
-
-})
+});
 
 export default router;
